e945035973a7f5424975e56360c9fca8f4b7f0a0
D M I <ademirjuniorfertonani@gmail.com>
2025-10-01 17:05:21 -0300

    exclusao do modulo de animals

--- src/animals/dto/create-animal.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateAnimalDto {
-  @IsNotEmpty({ message: 'O nome é obrigatório.' })
-  @IsString({ message: 'O nome deve ser uma string.' })
-  name: string;
-
-  
-}
